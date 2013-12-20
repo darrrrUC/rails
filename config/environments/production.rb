b5037189976d7913218996d55e405d9dac1fb974
Emil K. Onung <emilemacarlsson@gmail.com>
2013-12-20 01:27:34 +0100

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Omrails::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -44,7 +44,7 @@ Omrails::Application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
-config.assets.initialize_on_precompile=false
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
